c3dbff1124f829f7892cebaf5aff3458ccd8ad04
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-06-09 12:13:13 -0300

    atualização do script

--- script.js
+++ script.js
@@ -16,14 +16,43 @@ let posY = Math.floor(Math.random() * altura) - 90;
 posX = posX < 0 ? 0 : posX;
 posY = posY < 0 ? 0 : posY;
 
-console.log(posX, posY);
+// ---------------------------------------
 
 //Criação de elementos HTML
 let mosquito = document.createElement("img");
 mosquito.src = "./assets/mosca.png";
-mosquito.className = "mosquito-1";
+mosquito.className = mudaTamanho() + " " + ladoRandom();
 mosquito.style.left = posX + "px";
 mosquito.style.top = posY + "px";
 mosquito.style.position = "absolute";
 
 document.body.appendChild(mosquito);
+
+// ---------------------------------------
+
+// Função para aplicar a mudança na tela dos mosquitos, de forma randomina.
+function mudaTamanho() {
+  let classe = Math.floor(Math.random() * 3);
+
+  if (classe === 0) {
+    return "mosquito-1";
+  } else if (classe === 1) {
+    return "mosquito-2";
+  } else {
+    return "mosquito-3";
+  }
+}
+
+mudaTamanho();
+
+function ladoRandom() {
+  let classe = Math.floor(Math.random() * 2);
+
+  if (classe === 0) {
+    return "lado-a";
+  } else {
+    return "lado-b";
+  }
+}
+
+ladoRandom();
