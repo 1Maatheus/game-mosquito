33d5be3be797eb31fd099af12644067fff3efbce
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-06-09 14:35:46 -0300

    adicionado tela de inicio de jogo

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ function ajusteTela() {
 ajusteTela();
 // ------------------------------
 
+function iniciaJogo() {
+  let nivel = document.getElementById("nivel").value;
+
+  if (nivel === "") {
+    alert("Selecione um nível para iniciar o jogo.");
+    return false;
+  }
+}
+
 //Criação do cronômetro
 
 document.getElementById("cronometro").innerHTML = time;
